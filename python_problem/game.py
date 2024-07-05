7c1dd27696d0cbf791fd831e2d3ba6279a75898b
pyun seona <vustjsdk1@naver.com>
2024-07-05 22:46:17 +0900

    배스킨라빈스31-4단계

--- python_problem/game.py
+++ python_problem/game.py
@@ -6,7 +6,17 @@ def get_input():
             input_num = int(input("부를 숫자의 개수를 입력하세요(1, 2, 3만 입력 가능) : "))
             if 1 <= input_num <= 3:
                 return input_num
-            else
+            else:
                 print("1,2,3 중 하나를 입력하세요")
         except ValueError:
             print("정수를 입력하세요")
+            
+def player_turn(player_name, current_num):
+    count = get_input()
+    for i in range(1, count + 1):
+        current_num += 1
+        print(f"{player_name} : {current_num}")
+    return current_num
+
+player_name = "playerA"
+num = player_turn(player_name, num)
